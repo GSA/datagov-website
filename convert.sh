70e5cc33d629e96d124e29784ab52f529b464a18
robert-bryson <robert.bryson@gsa.gov>
2021-11-15 09:42:23 -0800

    Added more conversions

--- convert.sh
+++ convert.sh
@@ -12,7 +12,7 @@ fi
 echo Total occurrences of 'www.data.gov'
 grep -r $DIR -e 'www.data.gov' | wc -l
 
-# rename files type <link rel="stylesheet" href="http://www.data.gov/chosen.min.css?ver=1.0.0">
+# rename files type <link rel="stylesheet" href="http://www.data.gov/chosen.min.css@ver=1.0.0">
 echo Renaming files with @ver=x.x.x..
 echo    Total files to rename:
 find . -iname "*@ver=*" | wc -l 
@@ -22,18 +22,39 @@ find . -iname "*@ver=*" |
     mv $fname ${fname%@*}; 
     done
 
-# replace type <link rel="stylesheet" href="http://www.data.gov/chosen.min.css?ver=1.0.0">
+# replace type:
+# <link rel="stylesheet" href="wp/block-library/style.min.css@ver=5.5.6.css">
+echo Converting ver=x.x.x.css hrefs...
+find $DIR -type f -exec \
+    $SED -E --in-place \
+    's#(href=".*.css)(@ver=.*.css)#\1#gp' {} ';'
 
-# replace type <script type='text/javascript' src='http://www.data.gov/app/plugins/wp-open311/public/assets/js/chosen.jquery.min.js?ver=1.0.0'
+# replace type:
+# <script type='text/javascript' src='http://www.data.gov/app/plugins/wp-open311/public/assets/js/chosen.jquery.min.js?ver=1.0.0'
+echo Converting ver=x.x.x.js hrefs...
+find $DIR -type f -exec \
+    $SED -E --in-place \
+    's#(src=.*.js)(.ver=.*.js)#\1#gp' {} ';'
 
+# replace type:
+# wp-emoji-release.min.js?ver=5.5.6
+echo Converting wp-emoji-release.min.js?ver=x.x.x
+find $DIR -type f -exec \
+    $SED -E --in-place \
+    's#(wp-emoji-release.min.js)(.ver=.....)#\1#gp' {} ';'
+
+# replace type:
+# ( 'fetch' in window ) || document.write( '<script src="wp/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js@ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="wp/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js@ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="wp/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js@ver=2.0.2"></scr' + 'ipt>' );
+echo Converting wp-polyfill-fetch.min.js@ver=3.0.0\">\</scr\' + \'ipt>\'
+find $DIR -type f -exec \
+    $SED -E --in-place \
+    's#(src=".*?.js)(.ver=.*?")#\1#gp' {} ';'
 
 # replace type <a href="http://www.data.gov/energy-infrastructure"> 
 echo Converting \<a\> hrefs...
 find $DIR -type f -exec \
     $SED -E --in-place \
     's#href="https?://www.data.gov/#href="#g' {} ';'
-
-
 
 # Final count, note that some (many?) of these might be legitimate
 echo Total occurrences of 'www.data.gov'
