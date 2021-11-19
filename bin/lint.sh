0b21245a34636d63d84617fc1b0bb825e6078ae0
robert-bryson <91547795+robert-bryson@users.noreply.github.com>
2021-11-19 13:05:09 -0800

    Update bin/lint.sh
    Co-authored-by: Aaron D Borden <aaron.borden@gsa.gov>

--- bin/lint.sh
+++ bin/lint.sh
@@ -5,6 +5,7 @@
 # - strip comments starting with "#"
 # - strip trailing spaces
 # - delete blank lines
+# - concatenates each line into a comma-separated list (no spaces) for --file-ignore
 # This was written to be BSD + GNU sed compatible
 file_ignore=$(sed -E -e 's/#.*//' -e 's/\s\+$//' -e '/^\s*$/ d' .htmlproofer-ignore | paste -s -d , - )
 
