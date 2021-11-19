96c6daa7da8e001482d98dcc223816baeebd6551
Aaron D Borden <aaron.borden@gsa.gov>
2021-11-19 15:43:13 -0800

    Fix htmlproofer check-html issues

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Wrapper to provide an .htmlproofer-ignore for htmlproofer linting
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
 # - read .htmlproofer-ignore
 # - strip comments starting with "#"
 # - strip trailing spaces
@@ -9,4 +13,4 @@
 # This was written to be BSD + GNU sed compatible
 file_ignore=$(sed -E -e 's/#.*//' -e 's/\s\+$//' -e '/^\s*$/ d' .htmlproofer-ignore | paste -s -d , - )
 
-exec bundle exec htmlproofer --disable-external --file-ignore "${file_ignore}" _site
+exec bundle exec htmlproofer --check-html --disable-external --file-ignore "${file_ignore}" _site
